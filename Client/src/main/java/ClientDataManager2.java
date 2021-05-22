4cbc69046e49c62c9550cf8751c32eb35a2255a6
Maarten Mulders <mthmulders@apache.org>
2021-05-22 10:45:22 +0200

    Introduce package in shared module

--- Client/src/main/java/ClientDataManager2.java
+++ Client/src/main/java/ClientDataManager2.java
@@ -1,3 +1,5 @@
+import shared.DataStructure;
+
 public class ClientDataManager2 {
     public ClientDataManager2(){
         DataStructure data = new DataStructure();
--- Server/src/main/java/ServerDataManager/ServerDataManager.java
+++ Server/src/main/java/ServerDataManager/ServerDataManager.java
@@ -1,5 +1,7 @@
 package ServerDataManager;
 
+import shared.DataStructure;
+
 public class ServerDataManager {
     public ServerDataManager(){
         DataStructure data = new DataStructure();
